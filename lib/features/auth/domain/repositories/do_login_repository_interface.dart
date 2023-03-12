df3984ea6f437f425990922ae3be91a12dab4d78
MatheusFontenele <matheus.fontenele@icloud.com>
2023-03-11 23:11:34 -0300

    feat: criado o do login repository

--- lib/features/auth/domain/repositories/do_login_repository_interface.dart
+++ lib/features/auth/domain/repositories/do_login_repository_interface.dart
@@ -2,7 +2,7 @@ import 'package:clean_arch_flutter/core/error/failure.dart';
 import 'package:clean_arch_flutter/features/auth/domain/entities/user.dart';
 import 'package:dartz/dartz.dart';
 
-abstract class DoLoginRepository {
+abstract class DoLoginRepositoryInterface {
   Future<Either<Failure, User>> doLogin({
     required String email,
     required String password,
